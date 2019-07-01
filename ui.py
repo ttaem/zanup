70ca5d298e2afc7df7b99e145ddeb38ac7e40813
Lee Changkeel <cklee@innopiatech.com>
2019-07-01 13:18:25 +0000

    Add functions

--- ui.py
+++ ui.py
@@ -3,6 +3,9 @@ import tkinter.ttk as ttk
 from tkcalendar import Calendar, DateEntry
 
 
+def select_date():
+    print(calendar.selection_get())
+
 
 master = tk.Tk()
 
@@ -13,13 +16,15 @@ bottom_pane =  tk.LabelFrame(master, text='List of workday')
 upper_pane.pack(side=tk.TOP, fill=tk.BOTH)
 bottom_pane.pack(side=tk.TOP, fill=tk.BOTH)
 
-calendar = Calendar(upper_pane)
-calendar.pack(side=tk.LEFT, fill=tk.BOTH)
+calendar = Calendar(upper_pane, font="Arial 14")
+calendar.pack(side=tk.LEFT, fill=tk.BOTH, padx=10, pady=10)
 
+insert_button = tk.Button(upper_pane, text='Insert', width=30, command=select_date)
 delete_button = tk.Button(upper_pane, text='Delete', width=30, command=master.destroy)
 done_button = tk.Button(upper_pane, text='Generate', width=30, command=master.destroy)
 save_button = tk.Button(upper_pane, text='Save', width=30, command=master.destroy)
 
+insert_button.pack(fill=tk.X)
 delete_button.pack(fill=tk.X)
 done_button.pack(fill=tk.X)
 save_button.pack(fill=tk.X)
@@ -45,8 +50,14 @@ treeview.heading("End", text="End")
 
 treeview.column("#5", width=300, anchor="center")
 treeview.heading("Description", text="Description")
-treeview.pack()
+treeview.pack(fill=tk.BOTH)
 
+
+treeview.insert('','end', text='1', values=('2019-7-02', 'Sun', '09:00', '22:00', 'hahaha'))
+treeview.insert('','end', text='1', values=('2019-7-02', 'Sun', '09:00', '22:00', 'hahaha'))
+treeview.insert('','end', text='1', values=('2019-7-02', 'Sun', '09:00', '22:00', 'hahaha'))
+treeview.insert('','end', text='1', values=('2019-7-02', 'Sun', '09:00', '22:00', 'hahaha'))
+treeview.insert('','end', text='1', values=('2019-7-02', 'Sun', '09:00', '22:00', 'hahaha'))
 
 tk.mainloop()
 
