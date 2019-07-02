9aca9452bed702824319de70505aea0d904fc21a
Lee Changkeel <cklee@innopiatech.com>
2019-07-02 08:08:04 +0000

    Add insert pane

--- ui.py
+++ ui.py
@@ -12,12 +12,47 @@ master = tk.Tk()
 upper_pane = tk.PanedWindow(master)
 #bottom_pane =  tk.PanedWindow(master)
 bottom_pane =  tk.LabelFrame(master, text='List of workday')
+insert_pane = tk.LabelFrame(upper_pane, text='New Entry')
 
 upper_pane.pack(side=tk.TOP, fill=tk.BOTH)
 bottom_pane.pack(side=tk.TOP, fill=tk.BOTH)
 
 calendar = Calendar(upper_pane, font="Arial 14")
 calendar.pack(side=tk.LEFT, fill=tk.BOTH, padx=10, pady=10)
+
+insert_pane.pack(fill=tk.X)
+insert_pane.grid_rowconfigure(0, weight=1)
+insert_pane.grid_rowconfigure(1, weight=1)
+
+date_var = tk.StringVar(insert_pane)
+date_var.set("2019-07-03")
+date_label = tk.Label(insert_pane, textvariable=date_var)
+date_label.grid(row=0, column=0)
+#start_label = tk.Label(insert_pane, text="09:00")
+#start_label.grid(row=0, column=1)
+start_var = tk.StringVar(insert_pane)
+start_var.set('09')
+start_choices = {'07', '08', '09', '10', '11'}
+start_pop = tk.OptionMenu(insert_pane, start_var, *start_choices)
+start_pop.grid(row=0, column=1)
+
+during_label = tk.Label(insert_pane, text="~")
+during_label.grid(row=0, column=2)
+
+end_var = tk.StringVar(insert_pane)
+end_var.set('21')
+end_choices = {'21', '22', '23', '24'}
+end_pop = tk.OptionMenu(insert_pane, end_var, *end_choices)
+end_pop.grid(row=0, column=3)
+
+#end_label = tk.Label(insert_pane, text="22:00")
+#end_label.grid(row=0, column=2)
+level_label = tk.Label(insert_pane, text="A")
+#level_label.grid(row=0, column=3, sticky="E")
+level_label.grid(row=0, column=4)
+desc_text = tk.Text(insert_pane, height=2, width=50)
+desc_text.grid(row=1, columnspan=5)
+
 
 insert_button = tk.Button(upper_pane, text='Insert', width=30, command=select_date)
 delete_button = tk.Button(upper_pane, text='Delete', width=30, command=master.destroy)
