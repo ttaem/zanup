56683c00737dfd7c0a1d6cdee59637dfe719ab67
Lee Changkeel <cklee@innopiatech.com>
2019-07-03 10:16:32 +0000

    Add dataframe sample

--- ui.py
+++ ui.py
@@ -2,10 +2,23 @@ import tkinter as tk
 import tkinter.ttk as ttk
 from tkcalendar import Calendar, DateEntry
 
+import datetime
+import pandas as pd
 
-def select_date():
-    print(calendar.selection_get())
 
+def insert_row():
+    date = calendar.selection_get()
+    print(date)
+    print (calendar.get_date())
+    date_var.set(date)
+
+def cal_call(eventObject):
+    date = calendar.selection_get()
+    print(date)
+    print (date.weekday())
+    print (date.strftime('%a'))
+    print(eventObject)
+    date_var.set(date)
 
 master = tk.Tk()
 
@@ -17,24 +30,30 @@ insert_pane = tk.LabelFrame(upper_pane, text='New Entry')
 upper_pane.pack(side=tk.TOP, fill=tk.BOTH)
 bottom_pane.pack(side=tk.TOP, fill=tk.BOTH)
 
-calendar = Calendar(upper_pane, font="Arial 14")
+calendar = Calendar(upper_pane, font="Arial 14", firstweekday='sunday')
 calendar.pack(side=tk.LEFT, fill=tk.BOTH, padx=10, pady=10)
+calendar.bind("<<CalendarSelected>>", cal_call)
 
 insert_pane.pack(fill=tk.X)
 insert_pane.grid_rowconfigure(0, weight=1)
 insert_pane.grid_rowconfigure(1, weight=1)
 
 date_var = tk.StringVar(insert_pane)
-date_var.set("2019-07-03")
+date_var.set(datetime.datetime.today())
 date_label = tk.Label(insert_pane, textvariable=date_var)
 date_label.grid(row=0, column=0)
+
+week_var = tk.StringVar(insert_pane)
+#week_var.set("2019-07-03")
+week_label = tk.Label(insert_pane, textvariable=week_var)
+week_label.grid(row=0, column=1)
 #start_label = tk.Label(insert_pane, text="09:00")
 #start_label.grid(row=0, column=1)
 start_var = tk.StringVar(insert_pane)
 start_var.set('09')
 start_choices = {'07', '08', '09', '10', '11'}
 start_pop = tk.OptionMenu(insert_pane, start_var, *start_choices)
-start_pop.grid(row=0, column=1)
+start_pop.grid(row=0, column=2)
 
 during_label = tk.Label(insert_pane, text="~")
 during_label.grid(row=0, column=2)
@@ -54,7 +73,7 @@ desc_text = tk.Text(insert_pane, height=2, width=50)
 desc_text.grid(row=1, columnspan=5)
 
 
-insert_button = tk.Button(upper_pane, text='Insert', width=30, command=select_date)
+insert_button = tk.Button(upper_pane, text='Insert', width=30, command=insert_row)
 delete_button = tk.Button(upper_pane, text='Delete', width=30, command=master.destroy)
 done_button = tk.Button(upper_pane, text='Generate', width=30, command=master.destroy)
 save_button = tk.Button(upper_pane, text='Save', width=30, command=master.destroy)
@@ -65,8 +84,8 @@ done_button.pack(fill=tk.X)
 save_button.pack(fill=tk.X)
 
 treeview=ttk.Treeview(bottom_pane, 
-        columns=["Date", "Week", "Start", "End", "Description"],
-        displaycolumns=["Date", "Week", "Start", "End", "Description"])
+        columns=["Date", "Week", "Start", "End", "Rate", "Description"],
+        displaycolumns=["Date", "Week", "Start", "End", "Rate", "Description"])
 
 treeview.column("#0", width=30, anchor="center")
 treeview.heading("#0", text="#")
@@ -83,16 +102,18 @@ treeview.heading("Start", text="Start")
 treeview.column("#4", width=50, anchor="center")
 treeview.heading("End", text="End")
 
-treeview.column("#5", width=300, anchor="center")
+treeview.column("#5", width=30, anchor="center")
+treeview.heading("Rate", text="Rate")
+
+treeview.column("#6", width=300, anchor="center")
 treeview.heading("Description", text="Description")
 treeview.pack(fill=tk.BOTH)
 
+df = pd.read_csv("sample.csv")
+for row in df.itertuples(index=True, name='Pandas'):
+    treeview.insert('','end', text=row[0], values=row[1:])
 
-treeview.insert('','end', text='1', values=('2019-7-02', 'Sun', '09:00', '22:00', 'hahaha'))
-treeview.insert('','end', text='1', values=('2019-7-02', 'Sun', '09:00', '22:00', 'hahaha'))
-treeview.insert('','end', text='1', values=('2019-7-02', 'Sun', '09:00', '22:00', 'hahaha'))
-treeview.insert('','end', text='1', values=('2019-7-02', 'Sun', '09:00', '22:00', 'hahaha'))
-treeview.insert('','end', text='1', values=('2019-7-02', 'Sun', '09:00', '22:00', 'hahaha'))
+#treeview.insert('','end', text='1', values=('2019-7-02', 'Sun', '09:00', '22:00', 'hahaha'))
 
 tk.mainloop()
 
