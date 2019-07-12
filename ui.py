4833c4285fd824d095c4e29235b9831f628c4963
Lee Changkeel <cklee@innopiatech.com>
2019-07-12 15:50:23 +0900

    gen file name from month & pref, Error checking

--- ui.py
+++ ui.py
@@ -7,6 +7,7 @@ from tkinter import filedialog
 #import TkUtil.Dialog
 
 import datetime
+from datetime import date
 import pandas as pd
 
 import openpyxl as xl
@@ -15,11 +16,14 @@ from openpyxl.styles import Border, Side, Alignment
 from pathlib import Path
 import configparser
 
-name = "Lee Changkeel"
-position = "Senior"
-department = "1sil"
 
 g_config = configparser.ConfigParser()
+
+g_month = 1
+g_department = "1sil"
+g_name = "cklee"
+global df
+
 
 def ok_pref(pref_win):
     print("save...")
@@ -103,7 +107,7 @@ def create_pref_window():
     ok_button.pack(side=tk.TOP, fill=tk.X)
     cancel_button.pack(side=tk.TOP, fill=tk.X)
 
-def do_default_config():
+def read_config():
     my_file = Path("./config.ini")
     if my_file.is_file():
         print("config file already exist")
@@ -111,18 +115,34 @@ def do_default_config():
     else:
         print("config file not exist")
         #config = configparser.ConfigParser()
-        g_config['FILE'] = {'GenerationFilePath': '/home/cklee',
-                             'SaveFilePath': '/home/cklee'}
-        g_config['USER'] = {'Name': 'Lee Changkeel',
-                             'Position': 'Senior',
-                             'Department': '1sil'}
+        g_config['FILE'] = {'GenerationFilePath': './',
+                             'SaveFilePath': './'}
+        g_config['USER'] = {'Name': 'Hong gildong',
+                             'Position': 'bastard',
+                             'Department': 'Korea'}
         with open('config.ini', 'w') as configfile:
             g_config.write(configfile)
+
+def read_csv(month):
+    global df
+    filename_f = '{month}M_zanup_{department}_{name}.csv'
+    filename = filename_f.format(month=month, department=g_config['USER']['Department'], name=g_config['USER']['Name'])
+    print(filename)
+    my_file = Path(g_config['FILE']['SaveFilePath'] + filename)
+    if my_file.is_file():
+        df = pd.read_csv(my_file)
+    else:
+        df = pd.DataFrame(columns=['date', 'week', 'start', 'end', 'rate', 'desc'])
+
+
 
 
 def save_df():
     print("save")
-    df.to_csv("sample.csv", index=False)
+    filename_f = '{month}M_zanup_{department}_{name}.csv'
+    filename = filename_f.format(month=g_month, department=g_config['USER']['Department'], name=g_config['USER']['Name'])
+    print(filename)
+    df.to_csv(filename, index=False)
 
 
 def generate_xl():
@@ -154,14 +174,16 @@ def generate_xl():
 
     
     df_count = df.shape[0]
+    if df_count == 0:
+        return
     sheet.insert_rows(7, amount=df_count)
     #sheet["C30"].border=thin_border 
     print("insert " + str(df_count))
 
     for index, df_row in df.iterrows():
-        sheet["B"+str(index+7)] = name
-        sheet["C"+str(index+7)] = position
-        sheet["D"+str(index+7)] = department
+        sheet["B"+str(index+7)] = g_config['USER']['Name']
+        sheet["C"+str(index+7)] = g_config['USER']['Position']
+        sheet["D"+str(index+7)] = g_config['USER']['Department']
         sheet["E"+str(index+7)] = df_row['date']
         sheet["F"+str(index+7)] = df_row['week']
         sheet["G"+str(index+7)] = df_row['start']
@@ -193,10 +215,20 @@ def generate_xl():
 
     sheet["K"+str(index+7+1)] = "=SUM(K7:K" + str(index+7) + ")"
     sheet["K"+str(index+7+2)] = "=SUM(K" + str(index+7+1) + ")"
-    book.save("result.xlsx")
+
+    filename_f = '{month}M_zanup_{department}_{name}.xlsx'
+    filename = filename_f.format(month=g_month, department=g_config['USER']['Department'], name=g_config['USER']['Name'])
+    print(filename)
+
+    book.save(filename)
+
 
 def insert_row():
     global df
+
+    if len(desc_text.get("1.0", 'end-1c')) == 0:
+        msg.showerror("New Entry Insertion", "Please, Insert description!!!")
+        return
 
     for index, df_row in df.iterrows():
         print(date_var.get())
@@ -204,6 +236,7 @@ def insert_row():
         if date_var.get() == df_row['date']:
             msg.showerror("New Entry Insertion", "Duplicated Date Detected!!!")
             return
+
 
     df = df.append(pd.Series([date_var.get(), week_var.get(), start_var.get()+":00", end_var.get()+":00", rate_var.get(), desc_text.get("1.0", 'end-1c')], index=df.columns), ignore_index=True)
     print(df)
@@ -225,10 +258,17 @@ def insert_row():
 def delete_row():
     global df
     cur = treeview.focus()
+    if cur is None:
+        print("None just return")
     index = treeview.index(cur)
+    print("cur:")
     print(cur)
-    print(index)
+    print("index:" + str(index))
     print(df)
+    df_count = df.shape[0]
+    if (df_count == 0):
+        print("DF size is 0, just return")
+        return
     df = df.drop(df.index[index])
     print(df)
     df = df.reset_index(drop=True)
@@ -263,6 +303,14 @@ def directory_select(var):
     print(path)
     var.set(path)
 
+
+read_config()
+today = date.today()
+g_month = int(today.strftime('%m'))
+print(g_month)
+
+read_csv(g_month)
+
 master = tk.Tk()
 master.title("ZanUp")
 
@@ -289,7 +337,8 @@ master.config(menu=menu_bar)
 
 upper_pane = tk.PanedWindow(master)
 #bottom_pane =  tk.PanedWindow(master)
-bottom_pane =  tk.LabelFrame(master, text='List of workday')
+bottom_pane =  tk.LabelFrame(master, text="Work List", font="Arial 20")
+bottom_pane.configure(text="List of " + str(g_month)) 
 insert_pane = tk.LabelFrame(upper_pane, text='New Entry')
 
 upper_pane.pack(side=tk.TOP, fill=tk.BOTH)
@@ -321,7 +370,8 @@ week_label.grid(row=0, column=1, sticky='W')
 #start_label.grid(row=0, column=1)
 start_var = tk.StringVar(insert_pane)
 start_var.set('09')
-start_choices = {'07', '08', '09', '10', '11'}
+start_choices = {'07', '08', '09', '10', '11', '12'}
+start_choices = sorted(start_choices)
 start_pop = tk.OptionMenu(insert_pane, start_var, *start_choices)
 start_pop.grid(row=0, column=2, stick='W')
 
@@ -330,7 +380,8 @@ during_label.grid(row=0, column=3)
 
 end_var = tk.StringVar(insert_pane)
 end_var.set('21')
-end_choices = {'21', '22', '23', '24'}
+end_choices = {'16','17','18','19','20','21','22','23','24'}
+end_choices = sorted(end_choices)
 end_pop = tk.OptionMenu(insert_pane, end_var, *end_choices)
 end_pop.grid(row=0, column=4)
 
@@ -338,9 +389,11 @@ end_pop.grid(row=0, column=4)
 #end_label.grid(row=0, column=2)
 rate_var = tk.StringVar(insert_pane)
 rate_var.set('A')
-rate_label = tk.Label(insert_pane, textvariable=rate_var)
+rate_choices = {'A','B','C','D'}
+rate_choices = sorted(rate_choices)
+rate_pop  = tk.OptionMenu(insert_pane, rate_var, *rate_choices)
 #level_label.grid(row=0, column=3, sticky="E")
-rate_label.grid(row=0, column=5, padx=10)
+rate_pop.grid(row=0, column=5, padx=10)
 desc_var = tk.StringVar(insert_pane)
 desc_text = tk.Text(insert_pane, height=2, width=50)
 desc_text.grid(row=1, columnspan=6, sticky='WE')
@@ -384,7 +437,9 @@ treeview.pack(side='left', fill=tk.BOTH, expand=tk.YES)
 
 ttk.Style().configure('Treeview',rowheight=30)
 
-df = pd.read_csv("sample.csv")
+csv_file = Path("./config.ini")
+
+#df = pd.read_csv("sample.csv")
 for row in df.itertuples(index=True, name='Pandas'):
     treeview.insert('','end', text=row[0], values=row[1:])
 
@@ -400,7 +455,7 @@ filename = "sample.xlsx"
 book = xl.load_workbook(filename)
 sheet = book.worksheets[0]
 
-do_default_config()
+read_config()
 
 tk.mainloop()
 
